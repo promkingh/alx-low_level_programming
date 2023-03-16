667e3084221ffa5f970bddb9ea38828bb949edad
promkingh <iampromiseds@gmail.com>
2023-03-16 10:41:50 -0700

    Write a C program that prints a string and endwith a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{	/* String initialisation*/
+	char progr[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(progr);
+	return (0);
+}
