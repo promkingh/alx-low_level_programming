5695a7e9f63a167f67fd9063678534ec950d3b62
promkingh <iampromiseds@gmail.com>
2023-03-16 11:07:29 -0700

    Write a C program that prints a string with quotes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {	/* String initialisation*/
-	char progr[] = "Programming is like building a multilingual puzzle\n";
+	char progr[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(progr);
 	return (0);
