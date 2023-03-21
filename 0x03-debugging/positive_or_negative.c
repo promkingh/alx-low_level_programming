2ad64189bdeaed6f264edb7506b42ea47ccd99cb
promkingh <iampromiseds@gmail.com>
2023-03-21 02:54:39 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Determine the sign of a number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+	return (0);
+}
