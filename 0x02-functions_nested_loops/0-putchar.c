cd76008e508ccc167b39cbc730de8431dd9e4bb3
promkingh <iampromiseds@gmail.com>
2023-03-21 09:17:42 -0700

    A program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Print _putchar
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("%c%c%c%c%", 95, 112, 117, 116);
+	printf("%c%c%c%c%\n", 99, 104, 97, 114);
+	return (0);
+}
