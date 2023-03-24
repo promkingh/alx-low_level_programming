d2dd8c0476e4ca49c24baaf58f2c8ac59a96b7c4
Okwuchukwu Promise Igweh <iampromiseds@gmail.com>
2023-03-24 14:31:53 +0100

    Highest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- print largest prime factor of 612852475143
+ * Return: always 0 success
+ */
+int main(void)
+{
+  int n, l;
+  long long int num = 612852475143;
+
+  for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+			f = i;
+		}
+	}
+	printf("%lu ", f);
+	return (0);
+}
