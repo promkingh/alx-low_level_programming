df0c146a2f5288ce525e919a9c988372f97003dc
Okwuchukwu Promise Igweh <iampromiseds@gmail.com>
2023-03-24 23:22:05 +0100

    Print given numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,32 +8,27 @@
 
 void print_number(int n)
 {
+	
+	unsigned int n2, n1 = n;
+	int div = 1;
+
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
+		n1 = n * -1;
 	}
-	if (n >= 0)
+	n2 = n1;
+	while (n2 > 9)
 	{
-		if (n < 10)
-			_putchar(n + '0');
-		else if (n < 100)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		else if (n < 1000)
-		{
-			_putchar((n / 100) + '0');
-			_putchar(((n / 10) % 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		else if (n < 10000)
-		{
-			_putchar((n / 1000) + '0');
-			_putchar(((n / 100) % 10) + '0');
-			_putchar(((n / 10) % 10) + '0');
-			_putchar((n % 10) + '0');
-		}
+		div *= 10;
+		n2 = n2 / 10;
+	}
+	while (div >= 1)
+	{
+		n2 = n1 % div;
+		n1 /= div;
+		_putchar(n1 + '0');
+		n1 = n2;
+		div /= 10;
 	}
 }
