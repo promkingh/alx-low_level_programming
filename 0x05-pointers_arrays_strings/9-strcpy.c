6cbca862e22d5cf9cd59eef614412d1369e30c94
promkingh <iampromiseds@gmail.com>
2023-03-28 04:54:19 -0700

    All files added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string from source to destination
+ * @dest: destination to copy to
+ * @src: where to copy from
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
