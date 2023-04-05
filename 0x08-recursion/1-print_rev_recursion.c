552f5300c25fb344ddb2f36ca1be1ae460e4e813
promkingh <iampromiseds@gmail.com>
2023-04-05 08:53:30 -0700

    String Reversal Recursive function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev_recursion - print string in reverse recursively
+ * _print_rev_recursion - print string in reverse recursively
  * @s: input string
  * Return: Nothing
  */
@@ -10,9 +10,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
-	else
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
