8cc057d4e146d13cf8a101c09ccf017022755ffd
promkingh <iampromiseds@gmail.com>
2023-04-10 01:59:10 -0700

    Cents code fixed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,19 +10,33 @@
 
 int main(int argc, char *argv[])
 {
-	int coins, i, change;
+	int coins, change;
 	
 	int cents[] = {25, 10, 5, 2, 1};
 	coins = 0;
-	change = atoi(argv[i]);
 
-	if (argc != 2 || (change == 0 && argv[1] != '0')
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	while (change > 1)
+	change = atoi(argv[1]);
+	while (change > 0)
+	{
+		if (change >= cents[0])
+			change -= cents[0];
+		else if (change >= cents[1])
+			change -= cents[1];
+		else if (change >= cents[2])
+			change -= cents[2];
+		else if (change >= cents[3])
+			change -= cents[3];
+		else if (change >= cents[4])
+			change -= cents[4];
+		coins++;
+	}
+	printf("%d\n", coins);
 	
 	return (0);
 }
