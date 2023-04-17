4e1a6941324040e8d60df48c5b42741b3dcb247a
promkingh <iampromiseds@gmail.com>
2023-04-17 08:39:59 -0700

    Strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,36 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * words_count - counts the number of words in a given string
+ * @str: given string
+ * Return: pointer to the number
+ */
+
+int words_count(char *str)
+{
+	int k, l, nwrds;
+
+	k = 0;
+	nwrds = 0;
+
+	for (l = 0; str[l]; l++)
+	{
+		if (str[l] == ' ')
+			k = 0;
+		else if (l == 0)
+		{
+			k = 1;
+			nwrds++;
+		}
+	}
+	if (nwrds == 0)
+		return (NULL);
+
+	return (nwrd);
+}
+
 /**
  * strtow - splits a string into words
  * @str: string to be spilt
@@ -8,20 +38,43 @@
  */
 char **strtow(char *str)
 {
-	int k;
-	char **wds;
-	char *wd;
+	int arr_ind, words_ct, k, l, ct,  st, nd;
+	char **new_string, *temp_string;
 
-	wds = (char **)malloc(sizeof(char *));
-	wd = strtok(str, " ");
+	arr_c = 0;
 	k = 0;
-	while (*wd)
+	l = 0;
+
+	while (*(str + arr_ind))
+		arr_ind++;
+
+	new_string = (char **)malloc(sizeof(char *) * (words_count(*str) + 1));
+
+	if (new_string == NULL)
+		return (NULL);
+
+	for (ct = 0; ct < arr_ind; ct++)
 	{
-		wds[k] = wd;
-		k++;
-		wds = realloc(wds, sizeof(char *) * (k + 1));
-		wd = strtok('\0', " ");
+		if (str[ct] == ' ' || str[ct] == '\0')
+		{
+			if (k)
+			{
+				nd = ct;
+				temp_string = (char *)malloc(sizeof(char) * (k + 1));
+
+				if (temp_string == NULL)
+					return (NULL);
+				while (st < nd)
+					*tem_string++ = str[st++];
+				*temp_string = '\0';
+				new_string[l] = temp - k;
+				l++;
+				k = 0;
+			}
+		}
+		else if (k++ == 0)
+			st = ct;
 	}
-	wds[k] = '\0';
-	return (wds);
+	new_strings[l] =  NULL;
+	return (new_string);
 }
