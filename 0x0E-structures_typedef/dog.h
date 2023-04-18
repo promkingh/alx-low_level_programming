b1317edf9a7a58dfd4dbcad21da1a49579f2de90
promkingh <iampromiseds@gmail.com>
2023-04-18 15:56:11 -0700

    dog.h header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,12 +14,12 @@
  *		it's name, age and its' owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
