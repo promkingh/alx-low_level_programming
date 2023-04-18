58d87bf932dbc9a584909a8ce9effafd5b11620a
promkingh <iampromiseds@gmail.com>
2023-04-18 08:17:58 -0700

    Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - The dog's information
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: This details the dogs information which are
+ *		it's name, age and its' owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOH_H */
