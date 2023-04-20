d524ec0654dd9e6f92c9d24730948da8ad7cf9a8
promkingh <iampromiseds@gmail.com>
2023-04-20 05:37:18 -0700

    Iterator fxn

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - checks through an array and prints it
+ *			through a pointer function
+ * @array: given array pointer
+ * @size: size of the array
+ * @action: pointer to the funtion
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t k;
+
+	for (k = 0; k < size; k++)
+		action(array[k]);
+}
