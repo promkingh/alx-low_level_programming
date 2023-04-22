d94dbaebaa1dae3623bd6539dbfac85ce6e4bf7c
Okwuchukwu Promise Igweh <iampromiseds@gmail.com>
2023-04-22 02:24:30 +0100

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,89 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints all arguments based on format
- * @format: type of argument
+ * print_char - print a character
+ * @c: character
  *
  * Return: void
  */
+void print_char(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
 
+/**
+ * print_str - print a string
+ * @s: string
+ *
+ * Return: void
+ */
+void print_str(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_int - print an integer
+ * @i: integer
+ *
+ * Return: void
+ */
+void print_int(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_flt - print a float
+ * @f: float
+ *
+ * Return: void
+ */
+void print_flt(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_all - print all
+ * @format: list of argument types passed to the function
+ *
+ * Return: nothing
+ */
 void print_all(const char * const format, ...)
 {
+	unsigned int k, l;
+	prt all[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_int},
+		{"f", print_flt},
+		{NULL, NULL}
+	};
 	va_list args;
-	int fg;
-	const char *f = format;
-	char *sh;
+	char *separator = "";
 
 	va_start(args, format);
-	while (*f != '\0')
+	k = 0;
+	while (format && format[k])
 	{
-		switch (*f)
+		l = 0;
+		while (all[l].c)
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				fg = 0;
+			if (*(all[l].c) == format[k])
+			{
+				printf("%s", separator);
+				all[l].f(args);
+				separator = ", ";
 				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				fg = 0;
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				fg = 0;
-				break;
-			case 's':
-				sh = va_arg(args, char *);
-				if (!sh)
-					printf("(nil)");
-				else
-				{
-					printf("%s", sh);
-					fg = 0;
-				}
-				break;
-			default:
-				fg = 1;
-				break;
+			}
+			l++;
 		}
-		if (*(f + 1) != '\0' && fg == 0)
-			printf(", ");
-		f++;
+		k++;
 	}
 	va_end(args);
 	printf("\n");
