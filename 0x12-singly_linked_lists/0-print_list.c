46e0805dd197e242695c5bd5719098536a5449e0
promkingh <iampromiseds@gmail.com>
2023-04-28 15:59:30 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,8 +13,8 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		if ((*h).str == NULL)
+			printf("[0] (nil)");
 		else
 			printf("[%d] %s\n", (*h).len, (*h).str);
 		h = (*h).next;
