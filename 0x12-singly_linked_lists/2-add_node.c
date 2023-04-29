9025d96c6f1af988c061b4493acdc493c3d13f55
promkingh <iampromiseds@gmail.com>
2023-04-28 17:10:49 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,12 +16,14 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
+		free(new->str);
 		free(new);
 		return (NULL);
 	}
-	new->str = strdup(str);
-
 	for (i = 0; new->str[i]; i++)
 		len++;
 	new->len = len;
