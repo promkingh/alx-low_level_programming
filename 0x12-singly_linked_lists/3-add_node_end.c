b64183ff8726ba64ff1ddacc55b586fb8b22ee28
promkingh <iampromiseds@gmail.com>
2023-04-28 17:26:35 -0700

    Add node - end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,4 +10,35 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new, *new_head;
+	size_t i, len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new->str);
+		free(new);
+		return (NULL);
+	}
+
+	for (i = 0; new->str[i]; i++)
+		len++;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head != NULL)
+	{
+		new_head = *head;
+		while (new_head->next != NULL)
+			new_head = new_head->next;
+		new_head->next = new;
+	}
+	else
+		*head = new;
+
+	return (new);
+}
 
