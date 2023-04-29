4f95ae9302a2fabed353c8a8477ba848cade8297
promkingh <iampromiseds@gmail.com>
2023-04-29 02:16:01 -0700

    NASM

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+		extern printf		; declare printf
+
+
+section .text
+		global main		; entry point
+
+main:
+
+		mov	rdi, fmt		; format 
+		mov	rsi, msg		; argument string
+		mov	rax, 0			;
+		call	printf			; invoke printf
+
+		pop	rbp			; restore stack
+
+		mov	rax, 0
+		ret
+
+section .data
+msg:		db	"Hello, Holberton", 0	; argument string to print
+fmt:		db	"%s" , 10, 0		; format
