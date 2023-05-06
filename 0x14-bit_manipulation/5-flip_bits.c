68251525ee34b25da3c964beef2e03b28cda9ade
promkingh <iampromiseds@gmail.com>
2023-05-06 06:56:59 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,17 +11,15 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int diff;
-	unsigned int num;
+	unsigned long int diff, num;
 
 	diff = n ^ m;
 	num = 0;
 
 	while (diff > 0)
 	{
-		if (diff & 1)
-			num++;
+		num += diff & 1;
 		diff >>= 1;
 	}
-	return (diff);
+	return (num);
 }
