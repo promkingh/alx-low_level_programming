92cb94395b4dc80366b6f8eb8586c469433b4d2d
promkingh <iampromiseds@gmail.com>
2023-05-10 07:18:43 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,6 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
 	if (!text_content)
 		text_content = "";
 	len = 0;
