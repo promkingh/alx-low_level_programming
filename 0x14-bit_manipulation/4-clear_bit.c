1bcca51a86a0365c9ca115037c06c719f5acc5ca
promise <iampromiseds@gmail.com>
2023-09-28 21:27:13 +0100

    deleted

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: given integer
+ * @index: target index
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n = *n & (~(1 << index));
+	return (1);
+}
